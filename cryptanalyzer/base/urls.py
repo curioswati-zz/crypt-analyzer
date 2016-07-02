b124492670199477af4951157791f4e84a18b4cc
curioswati <jaiswalswati94@gmail.com>
2016-07-02 18:09:44 +0530

    adds logic to capture files and type of algorithm
    two different forms are posted to a single view, also initializes utility
    module for encryption

--- cryptanalyzer/base/urls.py
+++ cryptanalyzer/base/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^encryption/key_file/', views.EncryptionKey.as_view(), name='encryption_key'),
     url(r'^decryption/data_file/', views.DecryptionData.as_view(), name='decryption_data'),
     url(r'^decryption/key_file/', views.DecryptionKey.as_view(), name='decryption_key'),
-    url(r'^select_algorithm/', views.SelectAlgorithm.as_view(), name='select_algorithm'),
+    url(r'^select_algorithm/(?P<type>[\w]+)/', views.SelectAlgorithm, name='select_algorithm'),
     url(r'^$', views.IndexView.as_view(), name='index'),
 ]
--- cryptanalyzer/base/views.py
+++ cryptanalyzer/base/views.py
@@ -1,8 +1,10 @@
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views import generic
 
 from .forms import UploadFileForm, SelectAlgorithmForm
+from . import utils
 
 
 class IndexView(generic.TemplateView):
@@ -17,14 +19,6 @@ class EncryptionData(generic.FormView):
     form_class = UploadFileForm
     success_url = "base:select_algorithm"
     template_name = "encryption_data.html"
-
-    def form_valid(self, form):
-        data = []
-        for field in form.fields:
-            f = form[field].value()
-            data.append(f.read())
-
-        return HttpResponseRedirect(reverse_lazy(self.success_url))
 
 
 class EncryptionKey(generic.TemplateView):
@@ -49,10 +43,18 @@ class DecryptionKey(generic.TemplateView):
     template_name = "decryption_key.html"
 
 
-class SelectAlgorithm(generic.FormView):
-    form_class = SelectAlgorithmForm
-    success_url = "base:index"
-    template_name = "select_algorithm.html"
+files = []
 
-    def form_valid(self, form):
-        return HttpResponseRedirect(reverse_lazy(self.success_url))
+
+def SelectAlgorithm(request, type):
+    global files
+    if request.method == "POST" and request.FILES:
+        files = request.FILES
+        form = SelectAlgorithmForm()
+        return render(request, "select_algorithm.html", {'form': form})
+
+    elif request.method == "POST":
+        form = SelectAlgorithmForm(request.POST)
+        utils.encrypt(files, form['choice_field'].value())
+
+    return HttpResponseRedirect(reverse_lazy("base:index"))
