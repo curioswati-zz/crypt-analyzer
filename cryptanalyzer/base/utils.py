8c851517cb334cf7da6cb415bba11fc6c3d1824d
curioswati <jaiswalswati94@gmail.com>
2016-07-09 01:30:12 +0530

    separates cleaning of data and key, implements key varying analysis

--- cryptanalyzer/base/utils.py
+++ cryptanalyzer/base/utils.py
@@ -6,15 +6,35 @@ from Crypto.Cipher import Blowfish
 
 
 class BaseCipher():
-    def make_valid_input(self, in_str):
-        if len(in_str) % 16:
-            quotient = len(in_str) % 16
-            in_str += "X" * (16-quotient)
-        return in_str
+    def make_valid_text(self):
+        self.text = str(self.text)
+
+        if isinstance(self, AESCipher):
+            min_multiple = 16
+        else:
+            min_multiple = 8
+
+        quotient = len(self.text) % min_multiple
+        if quotient:
+            self.text += "X" * (min_multiple - quotient)
+
+    def make_valid_key(self):
+        self.key = str(self.key)
+
+        # set minimum allowed length of key
+        if isinstance(self, BlowfishCipher):
+            min_allowed_len = 4
+        else:
+            min_allowed_len = 16
+
+        min_multiple = int(min_allowed_len/2)
+        quotient = len(self.key) % min_multiple
+
+        if len(self.key) < min_allowed_len or quotient:
+            self.key += "X" * (min_multiple - quotient)
 
     def encrypt(self):
-        text = self.make_valid_input(self.text)
-        self.ciph = self.cipher_obj.encrypt(text)
+        self.ciph = self.cipher_obj.encrypt(self.text)
 
     def decrypt(self):
         return self.cipher_obj.decrypt(self.ciph)[:len(self.text)]
@@ -26,7 +46,7 @@ class AESCipher(BaseCipher):
         self.text = text
 
         # prepare cipher object
-        self.key = self.make_valid_input(self.key)
+        self.make_valid_key()
         self.cipher_obj = AES.new(self.key, AES.MODE_ECB)
 
 
@@ -36,7 +56,7 @@ class DES3Cipher(BaseCipher):
         self.text = text
 
         # prepare cipher object
-        self.key = self.make_valid_input(self.key)
+        self.make_valid_key()
         self.cipher_obj = DES3.new(self.key, DES3.MODE_ECB)
 
 
@@ -46,7 +66,7 @@ class BlowfishCipher(BaseCipher):
         self.text = text
 
         # prepare cipher object
-        self.key = self.make_valid_input(self.key)
+        self.make_valid_key()
         self.cipher_obj = Blowfish.new(self.key, Blowfish.MODE_ECB)
 
 
@@ -56,7 +76,7 @@ class TwofishCipher(BaseCipher):
         self.text = text
 
         # prepare cipher object
-        self.key = self.make_valid_input(self.key)
+        self.make_valid_key()
         self.cipher_obj = AES.new(self.key, AES.MODE_ECB)
 
 
@@ -66,7 +86,7 @@ class RC6Cipher(BaseCipher):
         self.text = text
 
         # prepare cipher object
-        self.key = self.make_valid_input(self.key)
+        self.make_valid_key()
         self.cipher_obj = AES.new(self.key, AES.MODE_ECB)
 
 
@@ -74,7 +94,7 @@ class Analyzer():
     def __init__(self):
         pass
 
-    def analyze(self, analysis_type, algorithms, key, files):
+    def analyze_varying_data(self, analysis_type, algorithms, key, files):
         algo_choices = {'aes': 'AESCipher',
                         'des': 'DES3Cipher',
                         'blowfish': 'BlowfishCipher',
@@ -90,6 +110,8 @@ class Analyzer():
 
                 for f in files:
                     algo_obj.text = f['content']
+                    algo_obj.make_valid_text()
+
                     key = algo+"_time"
                     if analysis_type == "encryption":
                         f[key] = self.calc_enc_time(algo_obj)
@@ -97,8 +119,34 @@ class Analyzer():
                     elif analysis_type == "decryption":
                         f[key] = self.calc_dec_time(algo_obj)
 
-        print(files)
         return files
+
+    def analyze_varying_key(self, analysis_type, algorithms, data, keys):
+        algo_choices = {'aes': 'AESCipher',
+                        'des': 'DES3Cipher',
+                        'blowfish': 'BlowfishCipher',
+                        'twofish': 'TwoCipher',
+                        'rc6': 'RCCipher'
+                        }
+
+        # iterate over the selected algorithms by user
+        for algo in algorithms:
+            if algo in algo_choices:
+                class_instance = eval(algo_choices[algo])
+
+                for keyfile in keys:
+                    stripped_key = keyfile['content'][2:-1]
+                    algo_obj = class_instance(key=stripped_key, text=data)
+                    algo_obj.make_valid_text()
+
+                    key = algo+"_time"
+                    if analysis_type == "encryption":
+                        keyfile[key] = self.calc_enc_time(algo_obj)
+
+                    elif analysis_type == "decryption":
+                        keyfile[key] = self.calc_dec_time(algo_obj)
+
+        return keys
 
     def calc_enc_time(self, algo_obj):
         taken_times = []
